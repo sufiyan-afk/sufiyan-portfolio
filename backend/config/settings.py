cca669b5111a25cf7bcfd73655ef0b05f2c9cb80
sufiyan-afk <widow5147@gmail.com>
2026-09-04 18:10:14 +0530

    Move Django secret key to environment variable

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*y2m*iznve7jkj3l@u59j=hqw-6$lh!(qa%e_uv7s*)uhxt9dj'
-
+SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
